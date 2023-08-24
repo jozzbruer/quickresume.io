a9b3ffc2c48644b24ac11ff281f8745a34be689c
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-08-23 18:01:12 -0700

    Update the responsiveness of the logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<Box bgcolor='#4951F5' minHeight='2rem' display='flex' alignItems='center'>
+		<Box bgcolor='#4951F5' height='3.2rem' display='flex' alignItems='center'>
 			<Container>
-				<Box color='#fff' fontSize='2rem' fontWeight='bold'>
+				<Box color='#fff' fontSize='1.7rem' fontWeight='bold'>
 					<p>QUICKRESUME.IO</p>
 				</Box>
 			</Container>
